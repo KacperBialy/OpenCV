7dcdbe10d9bfa7bd16ec6cad83b1db13bbd456cb
Kacper Biały <44371092+KacperBialy@users.noreply.github.com>
2020-04-18 16:53:16 +0200

    Update main.py

--- Detecting_colors/main.py
+++ Detecting_colors/main.py
@@ -15,8 +15,6 @@ cv2.createTrackbar("R", "Mask", 0, 255, nothing)
 cv2.createTrackbar("B_max", "Mask", 0, 255, nothing)
 cv2.createTrackbar("G_max", "Mask", 0, 255, nothing)
 cv2.createTrackbar("R_max", "Mask", 0, 255, nothing)
-cv2.createTrackbar("Morphology", "Mask", 0, 1, nothing)
-cv2.createTrackbar("kernel_morphology", "Mask", 0, 30, nothing)
 
 while True:
     _, frame = cap.read()
@@ -29,9 +27,6 @@ while True:
     R_max = cv2.getTrackbarPos("R_max", "Mask")
     G_max = cv2.getTrackbarPos("G_max", "Mask")
     B_max = cv2.getTrackbarPos("B_max", "Mask")
-
-    morphology_on_off = cv2.getTrackbarPos("Morphology", "Mask")
-    kernel_morphology = cv2.getTrackbarPos("kernel_morphology", "Mask")
 
     lower_red = np.array([B, G, R])
     upper_red = np.array([B_max, G_max, R_max])
